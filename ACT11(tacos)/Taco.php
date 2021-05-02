15110e27ad99b1acb841788b170556ae01d3ad6f
Alexa <alxa.fmc@gmail.com>
2021-05-01 20:20:08 -0500

    FEAT: agregue felicidades

--- ACT11(tacos)/Taco.php
+++ ACT11(tacos)/Taco.php
@@ -3,13 +3,16 @@
                  "Segundo"=>"B", 
                  "Tercero"=>"C",
                  "Cuarto"=>"D"];
+    
     $i=0;
     $a=0;
     $b=0;
     $c=0;
     $d=0;
 
+    
     $respuestas=[$_POST["p1"], $_POST["p2"], $_POST["p3"], $_POST["p4"], $_POST["p5"], $_POST["p6"], $_POST["p7"], $_POST["p8"], $_POST["p9"], $_POST["p10"]];
+
     for($i=0; $i < count($respuestas); $i++)
     {
         if($respuestas[$i]==$opciones["Primero"])
@@ -27,85 +30,29 @@
             $d++;
         }
     }
-    //condiciones para solo una respuesta > como condicion
+
     if($a>$b && $a>$c && $a>$d)
-        echo "<h1>";
-        echo "Felicidades eres un:"; 
-        echo "</h1>";
-        echo "<h2>";
-        echo "Taco al pastor";
-        echo "</h2>";
+        echo "Felicidades eres un: Taco al pastor";
     else if($b>$a && $b>$c && $b>$d)
-        echo "<h1>";
-        echo "Felicidades eres un:" ;
-        echo "</h1>";
-        echo "<h2>";
-        echo "Taco de suadero";
-        echo "</h2>";
+        echo "Felicidades eres un: Taco de suadero";
     else if($c>$a && $c>$b && $c>$d)
-        echo "<h1>";
-        echo "Felicidades eres un:" ;
-        echo "</h1>";
-        echo "<h2>";
-        echo "Taco de barbacoa";
-        echo "</h2>";
+        echo "Felicidades eres un: Taco de barbacoa";
     else if($d>$a && $d>$b && $d>$c)
-        echo "<h1>";
-        echo "Felicidades eres un:" ;
-        echo "</h1>";
-        echo "<h2>";
-        echo "Taco Lagunero";
-        echo "</h2>";
-    //condiciones para dos variables iuales
+        echo "Felicidades eres un: Taco Lagunero";
+
     else if($a==$b && $a>$c && $a>$d)
-        echo "<h1>";
-        echo "Felicidades eres un:" ;
-        echo "</h1>";
-        echo "<h2>";
-        echo "<Taco campechano";
-        echo"</h2>";
+        echo "Felicidades eres un: Taco campechano";
     else if($b==$c && $b>$a && $b>$d)
-        echo "<h1>";
-        echo "Felicidades eres un:" ;
-        echo "</h1>";
-        echo "<h2>";
-        echo "Taco de carnitas";
-        echo "</h2>";
+        echo "Felicidades eres un: Taco de carnitas";
     else if($c==$d && $c>$a && $c>$b)
-        echo "<h1>";
-        echo "Felicidades eres un:" ;
-        echo "</h1>";
-        echo "<h2>";
-        echo "Taco bell";
-        echo "</h2>";
+        echo "Felicidades eres un: Taco bell";
     else if($a==$d && $a>$c && $a>$b)
-        echo "<h1>";
-        echo "Felicidades eres un:" ;
-        echo "</h1>";
-        echo "<h2>";
-        echo "Taco light";
-        echo "</h2>";
+        echo "Felicidades eres un: Taco light";
     else if($a==$c && $a>$b && $a>$d)
-        echo "<h1>";
-        echo "Felicidades eres un:" ;
-        echo "</h1>";
-        echo "<h2>";
-        echo "Taco placero";
-        echo "</h2>";
+        echo "Felicidades eres un: Taco placero";
     else if($b==$d && $b>$c && $b>$a)
-        echo "<h1>";
-        echo "Felicidades eres un:" ;
-        echo "</h1>";
-        echo "<h2>";
-        echo "Taco de mixiote";
-        echo "</h2>";
-    //ninguna de las anteriores
+        echo "Felicidades eres un: Taco de mixiote";
     else
-        echo "<h1>";
-        echo "Felicidades eres un:" ;
-        echo "</h1>";
-        echo "<h2>";
-        echo "Taco de sal";
-        echo "</h2>";   
+        echo "Felicidades eres un: Taco de sal";   
     
 ?>
