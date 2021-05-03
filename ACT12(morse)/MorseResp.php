a75194a122a70846941522e0405a8d9d324a726b
Alexa <alxa.fmc@gmail.com>
2021-05-03 01:40:35 -0500

    FEAT: comentarios

--- ACT12(morse)/MorseResp.php
+++ ACT12(morse)/MorseResp.php
@@ -16,11 +16,15 @@
             ".----", "..---", "...--", "....-", ".....",
             "-....", "--...", "---..", "----.","-----",
             ".-.-.-", "-.-.--", "..--..", ".-..-.", "   ", " "];
-    
+    //guarda la peticion
     $textoE=$_POST['texto'];
+    //la vuelve mayuscula
     $textoEm=strtoupper($textoE);
+    // separa el texto por caracteres
     $separadoE= str_split($textoEm); 
+    //guarda la peticion
     $textoM=$_POST['texto'];
+    //separa por cada letra morse(espacio entre cada una)
     $separadoM= explode(" ", $textoM); 
 
 
@@ -29,9 +33,10 @@
     $k=0;
    
     
-    // condicionales de español a morse o de morse a español
+    // condicional de español a morse 
     if($_POST["traduccion"] == "espmor")
     {
+        // da el texto que introdujo el usuario
         echo "<h1>";
         echo "Tu texto a traducir es: ";
         echo "</h1>";
@@ -40,21 +45,26 @@
         echo "<h1>";
         echo "Tu texto traducido es: ";
         echo "</h1>";
+        // recorre el texto introducido por caracteres
         for($i=0; $i < count($separadoE); $i++)
         {
+           // recorre el abecedario
             for($j=0; $j < count($abecedario); $j++)
             {
+                //encuentra coincidencias entre el abecedario y el texto
                 if($abecedario[$j] == $separadoE[$i])
                 {
+                    // escribe las coincidencias traducidas
                     echo $morse[$j];
                     echo " ";
                 }
             }
+            // intento de poner los espacios :((
             if($separadoE[$i] == " ")
                 echo "   ";
+        }
     }
-    }
-
+    // condicional de morse a español
     elseif($_POST["traduccion"] == "moresp")
     {
         echo "<h1>";
@@ -65,15 +75,20 @@
         echo "<h1>";
         echo "Tu texto traducido es: ";
         echo "</h1>";
+        // recorre el texto dado
         for($i=0; $i < count($separadoM); $i++)
         {
+            // recorre abecedario morse
             for($j=0; $j < count($morse); $j++)
             {
-                if($morse[$j] == $separadoM[$i])
+            // encuentra coincidencias entre el abecedario morse y el texto
+            if($morse[$j] == $separadoM[$i])
                 {
+                    // imprime coincidencias en el otro abecedario
                     echo $abecedario[$j];
                 }
             }
+            // intento de diagonales :(((
             if($separadoM[$i] == " ")
                 echo "/";
 
