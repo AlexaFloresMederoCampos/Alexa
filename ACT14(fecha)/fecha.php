6452899b0b26b1adef6b25b435918ddedf5dbf6b
Alexa <alxa.fmc@gmail.com>
2021-05-05 21:52:19 -0500

    FEAT: arreglé codigo que tenia e

--- ACT14(fecha)/fecha.php
+++ ACT14(fecha)/fecha.php
@@ -1,23 +1,94 @@
 <?php
-
-    $hora;
-    $cumple;
-
-    if(isset($_POST["reloj"]) && isset($_POST["hora"]) && isset($_POST["paises"]))
+if(isset($_POST["enviar"])) //Boton procesar
+{
+    echo "<table border=1;>";
+echo "<thead>";
+            //titulo de la tabla
+             echo "<th colspan=2;><h1><strong>Reloj mundial</strong></h1></th>";
+           echo " </thead>";
+           echo " <tbody>";
+    if(isset($_POST["ny"]))
     {
-        $hora=$_POST["hora"];
-        echo $hora;    
-        echo "hola";
+        echo "<td colspan=1;>NewYork</td>";
+        echo "<td colspan=1;> lol </td>";
+    } 
+    if(isset($_POST["sp"]))
+    {
+        //echo $_POST["sp"];
+        echo "<tr>";
+        echo "<td colspan=1;>Sao Paolo</td>";
+        echo "<td colspan=1;> lol </td>";
+        echo "</tr>";
+    } 
+    if(isset($_POST["md"]))
+    {
+        //echo $_POST["md"];
+        echo "<tr>";
+        echo "<td colspan=1;>Madrid</td>";
+        echo "<td colspan=1;> lol </td>";
+        echo "</tr>";
     }
-    else
+    if(isset($_POST["pr"]))
     {
-        if(isset($_POST["cumpleanos"]) && isset($_POST["cumple"]) && isset($_POST["proxC"]))  
-        {
-            $cumple=$_POST["cumple"];
-            echo $cumple;
-            echo "cumple";    
-        }
-    }    
-    
-    
+       // echo $_POST["pr"];
+       echo "<tr>";
+       echo "<td colspan=1;>Paris</td>";
+       echo "<td colspan=1;> lol </td>";
+       echo "</tr>";
+    }
+    if(isset($_POST["rm"]))
+    {
+        //echo $_POST["rm"];
+        echo "<tr>";
+        echo "<td colspan=1;>Roma</td>";
+        echo "<td colspan=1;> lol </td>";
+        echo "</tr>";
+    }
+    if(isset($_POST["at"]))
+    {
+        //echo $_POST["at"];
+        echo "<tr>";
+        echo "<td colspan=1;>Atenas</td>";
+        echo "<td colspan=1;> lol </td>";
+        echo "</tr>";
+    }
+    if(isset($_POST["bj"]))
+    {
+        //echo $_POST["bj"];
+        echo "<tr>";
+        echo "<td colspan=1;>Bejin</td>";
+        echo "<td colspan=1;> lol </td>";
+        echo "</tr>";
+    }
+    if(isset($_POST["tk"]))
+    {
+        //echo $_POST["tk"];
+        echo "<tr>";
+        echo "<td colspan=1;>Tokio</td>";
+        echo "<td colspan=1;> lol </td>";
+        echo "</tr>";
+    }
+        echo "</table>";  
+}
+if(isset($_POST["enviar2"])) //Boton procesar
+{
+    if(isset($_POST["dias"]))
+    {
+        echo "dias";
+        echo "<br>";
+    } 
+    if(isset($_POST["horas"]))
+        echo "horas";
+        echo "<br>";
+         
+    if(isset($_POST["min"]))
+        echo "min";
+        echo "<br>";
+     
+    if(isset($_POST["fin"]))
+        echo "fin";
+        echo "<br>";
+}
+//}
+
 ?>
