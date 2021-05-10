e6f59c0955210425b451b2c784f533b476e5e949
Alexa <alxa.fmc@gmail.com>
2021-05-10 01:33:43 -0500

    FEAT: form correcto

--- ACT16(sesion)/form.php
+++ ACT16(sesion)/form.php
@@ -8,27 +8,31 @@
 <body>
     <?php
         session_start();
-        if(isset($_SESSION["msj"]))
+        if(isset($_SESSION["nombre"]))
         {
             header("location: ./index.php");
         }
-        echo '<form action="./index.php" method="POST">';
-            echo '<fieldset style="width=300">';
-                echo '<legend> <h1>Inicio de sesion</h1></legend>';
-                echo 'Nombre: <input type="text" name="nombre">';
-                echo '<br><br>';
-                echo 'Apellidos: <input type="text" name="apellidos">';
-                echo '<br><br>';
-                echo 'Grupo: <input type="text" name="grupo">';
-                echo '<br><br>';
-                echo 'Fecha de nacimiento: <input type="date" name="nacimiento">';
-                echo '<br><br>';
-                echo 'Correo electronico: <input type="mail" name="correo">';
-                echo '<br><br>';
-                echo 'Contraseña: <input type="text" name="contra">';
-                echo '<button type="submit" name="ingresar" value="ingresar">Ingresar</button>';
-            echo '</fieldset>';
-        echo '</form>';
+        else
+        {
+            echo '<form action="./index.php" method="POST">';
+                echo '<fieldset style="width=300">';
+                    echo '<legend> <h1>Inicio de sesion</h1></legend>';
+                    echo 'Nombre: <input type="text" name="nombre">';
+                    echo '<br><br>';
+                    echo 'Apellidos: <input type="text" name="apellidos">';
+                    echo '<br><br>';
+                    echo 'Grupo: <input type="text" name="grupo">';
+                    echo '<br><br>';
+                    echo 'Fecha de nacimiento: <input type="date" name="nacimiento">';
+                    echo '<br><br>';
+                    echo 'Correo electronico: <input type="mail" name="correo">';
+                    echo '<br><br>';
+                    echo 'Contraseña: <input type="password" name="contra">';
+                    echo '<button type="submit" value="ingresar">Ingresar</button>';
+                echo '</fieldset>';
+            echo '</form>';
+        }
+        
     ?>
     
 </body>
